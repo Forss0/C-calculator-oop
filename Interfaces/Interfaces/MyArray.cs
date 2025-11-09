6312d0aecc233ec3a0fcce65d5464bdba61e7737
Ivan <sanivan802@gmail.com>
2025-11-09 11:44:58 +0100

    Add files via upload

---
+++ Interfaces/Interfaces/MyArray.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyArray : IOutput, IMath, ISort
+{
+    private int[] arr;
+
+    public MyArray(int[] array)
+    {
+        arr = array;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Елементи масиву:");
+        foreach (var item in arr)
+            Console.Write(item + " ");
+        Console.WriteLine();
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine(info);
+        Show();
+    }
+
+    public int Max()
+    {
+        int max = arr[0];
+        foreach (var x in arr)
+            if (x > max) max = x;
+        return max;
+    }
+
+    public int Min()
+    {
+        int min = arr[0];
+        foreach (var x in arr)
+            if (x < min) min = x;
+        return min;
+    }
+
+    public float Avg()
+    {
+        float sum = 0;
+        foreach (var x in arr)
+            sum += x;
+        return sum / arr.Length;
+    }
+
+    public bool Search(int valueToSearch)
+    {
+        foreach (var x in arr)
+            if (x == valueToSearch)
+                return true;
+        return false;
+    }
+
+    public void SortAsc()
+    {
+        Array.Sort(arr);
+    }
+
+    public void SortDesc()
+    {
+        Array.Sort(arr);
+        Array.Reverse(arr);
+    }
+
+    public void SortByParam(bool isAsc)
+    {
+        if (isAsc)
+            SortAsc();
+        else
+            SortDesc();
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        int[] data = { 3, 2, 4, 1, 5 };
+        MyArray arr = new MyArray(data);
+
+        arr.Show();
+        arr.Show("Вивід масиву з повідомленням:");
+
+
+        Console.WriteLine("\nМаксимум: " + arr.Max());
+        Console.WriteLine("Мінімум: " + arr.Min());
+        Console.WriteLine("Середнє: " + arr.Avg());
+        Console.WriteLine("Пошук 2: " + arr.Search(2));
+        Console.WriteLine("Пошук 100: " + arr.Search(100));
+
+        Console.WriteLine("\nСортування за зростанням:");
+        arr.SortAsc();
+        arr.Show();
+
+        Console.WriteLine("Сортування за спаданням:");
+        arr.SortDesc();
+        arr.Show();
+
+        Console.WriteLine("Сортування SortByParam(true):");
+        arr.SortByParam(true);
+        arr.Show();
+
+        Console.WriteLine("Сортування SortByParam(false):");
+        arr.SortByParam(false);
+        arr.Show();
+    }
+}
